64147d4a9b3d18fdb532415eca92dbae7ca5eba3
Wes <wes_peacock@sil.org>
2023-05-18 11:43:52 -0600

    Add the script and expand the README

---
+++ AddLineNumber.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/env perl
+my $USAGE = "Usage: $0 [--flgmarks lx] [--help] [--debug] [file.sfm]";
+
+use 5.022;
+use utf8;
+use open qw/:std :utf8/;
+
+use strict;
+use warnings;
+use English;
+use File::Basename;
+my $scriptname = fileparse($0, qr/\.[^.]*/); # script name without the .pl
+use Getopt::Long;
+GetOptions (
+	'flgmarks:s' => \(my $flgmarks = "lx"), # record marker, default lx
+# 'sampleoption:s' => \(my $sampleoption = "optiondefault"),
+# additional options go here.
+
+	# Be aware # is the bash comment character, so quote it if you want to specify it.
+	#	Better yet, just don't specify it -- it's the default.
+	'help'    => \my $help,
+	'debug'       => \my $debug
+	) or die $USAGE;
+
+if ($help) {
+	say STDERR $USAGE;
+	say STDERR 'Add a line number to the end of SFM field(s).';
+	say STDERR 'The flgmarks option specifies which marker(s) receive the line number';
+	say STDERR 'The line number is six digits with leading zeros, preceded by a colon.';
+	say STDERR 'If the field has a previous line number, it is replaced with the new number';
+	exit;
+	}
+for ($flgmarks) {
+	# remove backslashes and spaces from the SFMs
+	say STDERR $_ if $debug;
+	$flgmarks =~ s/\\//g;
+	$flgmarks =~ s/ //g;
+	$flgmarks =~ s/\,*$//; # no trailing commas
+	$flgmarks =~ s/\,/\|/g;  # use bars for or'ing
+	}
+my $srchflgmarks = qr/$flgmarks/;
+my $ix;
+
+LINE:
+while (<>) {
+	$ix = sprintf('%06d', $.);
+	if (/^\\$srchflgmarks /) {
+		s/\R//;
+		s/\:[0-9]{6}$//; # remove existing line number
+		s/$/:$ix\n/
+		}
+	}
+continue {
+      print or die "-p destination: $!\n";
+  }
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Add Line Numbers to a Standard Format file
  A perl script to append line numbers to certain fields in a Standard Format file.
 
+Usage:
+
+	./AddLineNumber.pl [--flgmarks lx] [--help] [--debug] [file.sfm]
+
+The SFM fields in the flgmarks list will have a colon and a six digit line number with leading zeros appended to them.
